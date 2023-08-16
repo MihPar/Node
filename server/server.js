d385aa1b6ab2be2cf2ec2ee3e8d8b0ea22420859
MihPar <9496260@gmail.com>
2023-08-16 12:25:46 +0300

    server03

--- server/server.js
+++ server/server.js
@@ -1,31 +1,49 @@
 const http = require('http')
 const fs = require('fs')
-const {readFile} = require('node:fs/promises')
 
-let count = 0
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+} 
+
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, function(err, data) {
+            if(err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
 const server = http.createServer(async function(req, res) {
-    // count++
-    // switch(req.url) {
-    //     case '/favicon.ico':
-    //         res.writeHead(200, {"Content-Type": 'image/x-icon'})
-    //         const context = await readFile('./favicon.ico', {encoding: 'utf8'})
-    //         res.end(context)
-    //     case '/':
-    //     case '/home':
-    //         const content = "best free only course"
-    //         res.write(content)
-    //         res.end()
-    //         break
-    //     default: 
-    //         res.write("404 not found")
-    //         res.end()
-    // }
-    // res.write(count + "Hello Node.js")
-    const result = function() {
-        return count++
+    switch(req.url){
+        case "/home":
+            try {
+                const data = await readFile('server/home1.html')
+                res.write(data)
+                res.end()
+            } catch(err) {
+                res.write("somethingh wrong")
+                res.end()
+            }
+            
+            break
+        case '/about':
+            await delay(3000)
+                res.write('ABOUT COURSE')
+                res.end()
+            break
+        default:
+            res.write('404 not found')
+            res.end()
     }
-    res.end("Hello Node.js: " + result())
 })
 server.listen(3000, '127.0.0.1', function() {
-    console.log("Server was started at port 3000 ")
+    console.log('Server was started at port 3000')
 })
