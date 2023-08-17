246071ab8f57eb147c1f85f9b682001294c88e83
MihPar <9496260@gmail.com>
2023-08-18 00:08:20 +0300

    favicon

--- server/script05.js
+++ server/script05.js
@@ -1,51 +1,42 @@
 const http = require('http')
+const favicon = require('serve-favicon')
+const path = require('path')
+const finalhandler = require('finalhandler')
 
-// let PORT = process.argv || 3000
-let couter = 0
+let _favicon = favicon(path.join(__dirname, 'favicon.ico'))
+let count = 0
+
 const server = http.createServer(function(request, response) {
-    // couter++
-    response.write('Node.js: ' + couter)
-    response.end()
-})  
-server.listen(3000, '127.0.0.1', function() {console.log(`Server was started at 3000`)})
+    count++
+    const done = finalhandler(request, response)
 
+    _favicon(request, response, function(err) {
+        if(err) {
+            return done(err)
+        } else {
+            switch(request.url) {
+                case '/friends':
+                    response.write('Hello my friends: ' + count)
+                    // response.end()
+                    break
+                case '/my_family':
+                    response.write('My diar family, I love you: ' + count)
+                    // response.end()
+                    break
+                case '/my_wife':
+                    response.write('I love you, my wify: ' + count)
+                    // response.end()
+                    break
+                default:
+                    response.write('404 not found ')
+                    // response.end()
+            }
+            response.write('Hello Node.js: ' + count)
+            response.end()
+        }
+    })
+})
 
-    // switch(request.url) {
-    //     case '/students':
-    //         response.write('STUDENTS')
-    //         // response.end()
-    //         break
-    //     case '/':
-    //     case '/courses':
-    //         response.write('FROM ' + 'BACK')
-    //         // response.end()
-    //         break
-    //     default:
-    //         response.write('404 not found')
-    //         // response.end()
-    // }
-    // response.write(' Hello Node.js ' + counter)
-    // response.end()
-
-
-// var http = require('http')
-// var favicon = require('serve-favicon')
-// var finalhandler = require('finalhandler')
-// var path = require('path')
-
-// var _favicon = favicon(path.join(__dirname, 'public', 'favicon.ico'))
-
-// var server = http.createServer(function onRequest (req, res) {
-//   var done = finalhandler(req, res)
-
-//   _favicon(req, res, function onNext (err) {
-//     if (err) return done(err)
-
-//     // continue to process the request here, etc.
-
-//     res.statusCode = 404
-//     res.end('oops')
-//   })
-// })
-
-// server.listen(3000)
+server.listen(3000, '127.0.0.1', function() {
+    console.log('Server was started at port 3000')
+})
